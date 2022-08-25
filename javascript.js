aa6f9d1137056870799a0231281828908450a7cb
Chanatinart <gift_nk_61@hotmail.com>
2022-08-26 00:14:15 +0700

    ver1

---
+++ javascript.js
@@ -0,0 +1,100 @@
+//data structures
+
+let myLibrary = [];
+
+class Book {
+    constructor(title ,author ,pages ,status) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.status = status;
+    }
+}
+
+//user interface
+const addBookBtn = document.getElementById('addBookBtn')
+const addBookModal = document.getElementById('addBookModal')
+
+
+//get add book modal
+addBookBtn.onclick = function() {
+    addBookModal.style.display = 'block';
+}
+
+window.addEventListener("click", function(e){
+    if (e.target == addBookModal) {
+        addBookModal.style.display = "none";
+    }
+});
+
+
+
+// get book from local storage
+if (localStorage.getItem('books')=== null) {
+    myLibrary = [];
+} else {
+    const booksFromStorage = JSON.parse(localStorage.getItem('books'));
+    myLibrary = booksFromStorage;
+}
+
+function showBookInLibrary() {
+    localStorage.setItem('books', JSON.stringify(myLibrary));
+    const bookList = document.querySelector('#book-table-body');
+    bookList.textContent = '';
+    for (let i = 0; i < myLibrary.length; i+= 1) {
+        const bookRow = document.createElement('tr');
+        bookRow.classList.add('book-info');
+        bookList.appendChild('bookRow');
+
+        //book title
+        const bookTitle = document.createElement('td');
+        bookTitle.textContent = myLibrary[i].title;
+        bookRow.appendChild('bookTitle');
+
+        //book author
+       const bookAuthor = document.createElement('td');
+        bookAuthor.textContent = myLibrary[i].author;
+        bookRow.appendChild('bookAuthor');
+
+        //book pages
+        const bookPages = document.createElement('td');
+        bookPages.textContent = myLibrary[i].pages;
+        bookRow.appendChild('bookPages');
+
+        //book status
+        const bookStatus = document.createElement('td');
+        if (myLibrary.status === false) {
+            bookStatus.textContent = 'read'
+        } else {
+            bookStatus.textContent = 'Not read'
+        }
+        bookRow.appendChild(bookStatus);
+
+        //book remove
+        const bookRemove = document.createElement('td');
+
+
+
+
+    }
+}
+
+function addBookToLibrary(title ,author ,pages ,status){
+    const book = new Book(title ,author ,pages ,status);
+    myLibrary.push(book);
+    showBookInLibrary();
+}
+
+//form
+
+    const titleInput = document.getElementById('title');
+    const inputAuthor = document.getElementById('author');
+    const inputPages = document.getElementById('pages');
+    const status = document.getElementById('isRead');
+    const tableBody = document.getElementById('book-table-body');
+    const addBookForm = document.getElementById('addBookForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    addBookToLibrary();})
+
+
+    
